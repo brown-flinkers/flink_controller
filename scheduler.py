49e0de70617313c5df8265906a83e1a0801efebf
QC Yang <qingchuanyang28@gmail.com>
2023-07-11 14:42:37 -0400

    scheduler work!

--- scheduler.py
+++ scheduler.py
@@ -7,13 +7,26 @@ import subprocess
 import os
 import traceback
 from collections import defaultdict
+import threading
 
 avg_over_time = "30s"
 prometheus_address = "localhost:9090"
 query = "word_count"
 flink_rest_api = "http://localhost:8081"
 cool_down = 240
+overprovisioning_factor = 1
+ops = ['Source:_Source_One', 'Source:_Source_Two', 'FlatMap_tokenizer', 'Count_op', 'Sink:_Dummy_Sink']
 
+
+def find_running_job():
+    job_id = ''
+    job_id_json = requests.get(f"{flink_rest_api}/jobs/")
+    for job in job_id_json.json()['jobs']:
+        if job['status'] == 'RUNNING':
+            job_id = job['id']
+
+    assert job_id != ''
+    return job_id
 
 def extract_per_operator_metrics(metrics_json, include_subtask=False):
     metrics = metrics_json.json()["data"]["result"]
@@ -65,7 +78,61 @@ def generate_graph(json_data):
 
 class scheduler:
 
+    def call_ds2(self):
+        print("---Calling DS2---")
+
+        command = [
+            "cargo",
+            "run",
+            "--manifest-path",
+            "./ds2/controller/Cargo.toml",
+            "--release",
+            "--bin",
+            "policy",
+            "--",
+            "--topo",
+            "ds2_query_data/flink_topology_" + query + ".csv",
+            "--rates",
+            "ds2_query_data/flink_rates_" + query + ".log",
+            "--system",
+            "flink"
+        ]
+
+
+        # parse the result of DS2
+        ds2_model_result = subprocess.run(command, capture_output=True)
+        output_text = ds2_model_result.stdout.decode("utf-8")
+        print(output_text)
+        output_text_values = output_text.split("\n")[-2]
+        output_text_values = output_text_values[1:-1]
+
+        suggested_parallelism = {}
+
+        filtered = []
+        for val in output_text_values.split(','):
+            if "topic" not in val:
+                val = val.replace(" NodeIndex(0)\"", "")
+                val = val.replace(" NodeIndex(4)\"", "")
+                filtered.append(val)
+
+        for i in range(0, len(filtered), 2):
+            suggested_parallelism[filtered[i]] = math.ceil(float(filtered[i + 1].replace("\"", ""))*overprovisioning_factor)
+        if suggested_parallelism[filtered[i]] == 0:
+            raise Exception("parallelism is zero")
+        if suggested_parallelism[filtered[i]] > 16:
+            suggested_parallelism[filtered[i]] = 16
+        if suggested_parallelism[filtered[i]] <= 0:
+            suggested_parallelism[filtered[i]] = 1
+
+        print("---Calling DS2---DONE")
+        print("NEW parallelism: ")
+        print(suggested_parallelism)
+
+        return suggested_parallelism
+
     def collect_and_write_data_to_file(self):
+        print("---Collecting and Writing Metrics---")
+
         input_rate_query = requests.get(
             "http://" + prometheus_address + "/api/v1/query?query=rate(flink_taskmanager_job_task_numRecordsInPerSecond[" + avg_over_time + "])")
         output_rate_query = requests.get(
@@ -82,7 +149,6 @@ class scheduler:
             "http://" + prometheus_address + "/api/v1/query?query=sum(flink_taskmanager_job_task_operator_KafkaSourceReader_KafkaConsumer_records_lag_max * flink_taskmanager_job_task_operator_KafkaSourceReader_KafkaConsumer_assigned_partitions) by (task_name)")
 
 
-
         input_rates_per_operator = extract_per_operator_metrics(input_rate_query, include_subtask=True)
         output_rates_per_operator = extract_per_operator_metrics(output_rate_query, include_subtask=True)
         busy_time_per_operator = extract_per_operator_metrics(busy_time_query, include_subtask=True)
@@ -95,22 +161,15 @@ class scheduler:
         input_rate_kafka = extract_per_operator_metrics(input_rate_kafka)
         lag = extract_per_operator_metrics(lag)
 
-        # print("Obtained metrics")
-        # print(operators)
-        #
-        # print(input_rates_per_operator)
-        # print(output_rates_per_operator)
-        # print(busy_time_per_operator)
-
         true_processing_rate = {}
         for key in input_rates_per_operator:
             true_processing_rate[key] = input_rates_per_operator[key] / (busy_time_per_operator[key] / 1000)
-            print("Operator: " + key + " InputRate: " + str(true_processing_rate[key]))
+            # print("Operator: " + key + " InputRate: " + str(true_processing_rate[key]))
 
         true_output_rate = {}
         for key in output_rates_per_operator:
             true_output_rate[key] = output_rates_per_operator[key] / (busy_time_per_operator[key] / 1000)
-            print("Operator: " + key + " OutputRate: " + str(true_output_rate[key]))
+            # print("Operator: " + key + " OutputRate: " + str(true_output_rate[key]))
 
         with open('./ds2_query_data/flink_rates_' + query + '.log', 'w+', newline='') as f:
             writer = csv.writer(f)
@@ -128,12 +187,9 @@ class scheduler:
                 row = [operator, operator_id, int(processors_per_operator[operator]), timestamp, true_processing_rate[key], true_output_rate[key], input_rates_per_operator[key], output_rates_per_operator[key]]
                 print(row)
                 writer.writerow(row)
-        print("Wrote rates file")
+        # print("Wrote rates file")
 
-        job_id_json = requests.get(f"{flink_rest_api}/jobs/")
-        job_id = job_id_json.json()['jobs'][0]['id']
-        print(job_id)
-
+        job_id = find_running_job()
         plan = requests.get(f"{flink_rest_api}/jobs/{job_id}/plan")
         graph = generate_graph(plan.json())
 
@@ -153,40 +209,60 @@ class scheduler:
                     row = [k[0].replace(' ', '_'), k[0].replace(' ', '_'), k[1], node[0].replace(' ', '_'), node[0].replace(' ', '_'), node[1]]
                     writer.writerow(row)
 
-        print("Wrote topology file")
+        print("---Collecting and Writing Metrics---DONE")
+        print("OLD parallelism: ")
         print(processors_per_operator)
 
-        # command = " ".join(["cargo", "run", "--manifest-path", "./ds2/controller/Cargo.toml", "--release", "--bin", "policy", "--", "--topo", "ds2_query_data/flink_topology_" + query + ".csv", "--rates", "ds2_query_data/flink_rates_" + query + ".log", "--system", "flink"])
-        # print(command)
-        # cargo run --manifest-path ./ds2/controller/Cargo.toml --release --bin policy -- --topo ds2_query_data/flink_topology_word_count.csv --rates ds2_query_data/flink_rates_word_count.log --system flink
+    def restart(self, path_to_savepoint, new_parallelism):
+        def restart_job():
+            print("---Restarting Job---")
+            command = [
+                'docker', 'exec', '-it', 'jobmanager', './bin/flink', 'run',
+                '-s', path_to_savepoint,
+                '--class', 'ch.ethz.systems.strymon.ds2.flink.wordcount.TwoInputsWordCount',
+                'target_jar',
+                '--p1', str(1 if not ops[0] in new_parallelism else new_parallelism[ops[0]]),
+                '--p2', str(1 if not ops[1] in new_parallelism else new_parallelism[ops[1]]),
+                '--p3', str(1 if not ops[2] in new_parallelism else new_parallelism[ops[2]]),
+                '--p4', str(1 if not ops[3] in new_parallelism else new_parallelism[ops[3]]),
+                '--p5', str(1 if not ops[4] in new_parallelism else new_parallelism[ops[4]])
+            ]
+            subprocess.Popen(command)
+            print("---Restarting Job DONE---")
 
+        # Create a thread for the restart_job function
+        thread = threading.Thread(target=restart_job)
 
-        ds2_model_result = subprocess.run(["cargo", "run", "--manifest-path", "./ds2/controller/Cargo.toml", "--release", "--bin", "policy", "--", "--topo", "ds2_query_data/flink_topology_" + query + ".csv", "--rates", "ds2_query_data/flink_rates_" + query + ".log", "--system", "flink"], capture_output=True)
-        output_text = ds2_model_result.stdout.decode("utf-8").replace("\n", "")
-        output_text_values = output_text.split(",")
-        suggested_parallelism = {}
+        # Start the thread
+        thread.start()
 
-        print(output_text_values)
+    def stop(self, job_id):
+        # stop
+        print("---Stopping Job---")
+        stop_job_url = f"{flink_rest_api}/jobs/{job_id}/stop"
+        stop_request = requests.post(stop_job_url)
 
+        job_status_url = f"{flink_rest_api}/jobs/{job_id}"
+        job_running = True
 
+        while job_running:
+            time.sleep(1)  # Wait for 1 second before checking the status again
+            status_request = requests.get(job_status_url)
+            status_data = status_request.json()
+            job_status = status_data.get("state", "UNKNOWN")
 
-    def restart(self, path_to_savepoint):
-        command = [
-            'docker', 'exec', '-it', 'jobmanager', './bin/flink', 'run',
-            '-s', path_to_savepoint,
-            '--class', 'ch.ethz.systems.strymon.ds2.flink.wordcount.TwoInputsWordCount',
-            'target_jar',
-            '--p1', str(2),
-            '--p2', str(2),
-            '--p3', str(2)
-        ]
-        subprocess.run(command)
+            if job_status == "RUNNING":
+                print("Job is still running...")
+            else:
+                print(f"Job status: {job_status}")
+                job_running = False
+
+        print("---Stopping Job DONE---")
 
     def take_savepoint(self):
+        print("---Taking Savepoint---")
         # get the job id
-        job_id_json = requests.get(f"{flink_rest_api}/jobs/")
-        job_id = job_id_json.json()['jobs'][0]['id']
-        print(job_id)
+        job_id = find_running_job()
 
         # take savepoint
         trigger_savepoint_url = f"{flink_rest_api}/jobs/{job_id}/savepoints"
@@ -200,160 +276,32 @@ class scheduler:
         savepoint_status_url = f"{flink_rest_api}/jobs/{job_id}/savepoints/{trigger_id}"
         response = requests.get(savepoint_status_url)
         status = response.json()["status"]["id"]
+        savepoint_name = requests.get(f"{flink_rest_api}/jobs/{job_id}/savepoints/{trigger_id}")
 
-        while status == "IN_PROGRESS":
+        while status == "IN_PROGRESS" or "location" not in savepoint_name.json()["operation"]:
             response = requests.get(savepoint_status_url)
             status = response.json()["status"]["id"]
+
+            savepoint_name = requests.get(f"{flink_rest_api}/jobs/{job_id}/savepoints/{trigger_id}")
             time.sleep(10)
 
         assert status == "COMPLETED"
 
-        savepoint_name = requests.get(f"{flink_rest_api}/jobs/{job_id}/savepoints/{trigger_id}")
         print(savepoint_name.json())
         savepoint_path = savepoint_name.json()["operation"]["location"]
         print(savepoint_path)
 
-        # stop
-        stop_job_url = f"{flink_rest_api}/jobs/{job_id}/stop"
-        stop_request = requests.post(stop_job_url)
-        print(stop_request)
+        print("---Taking Savepoint DONE---")
+        return job_id, savepoint_path
 
-        return savepoint_path
-
-    def start(self):
+    def run(self):
         while True:
-            input_rate_query = requests.get(
-                "http://" + prometheus_address + "/api/v1/query?query=rate(flink_taskmanager_job_task_operator_numRecordsIn[" + avg_over_time + "])")
-            output_rate_query = requests.get(
-                "http://" + prometheus_address + "/api/v1/query?query=avg_over_time(flink_taskmanager_job_task_numRecordsOutPerSecond[" + avg_over_time + "])")
-            busy_time_query = requests.get(
-                "http://" + prometheus_address + "/api/v1/query?query=avg_over_time(flink_taskmanager_job_task_busyTimeMsPerSecond[" + avg_over_time + "])")
-            number_of_processors_per_task = requests.get(
-                "http://" + prometheus_address + "/api/v1/query?query=count(flink_taskmanager_job_task_operator_numRecordsIn) by (task_name)")
-            input_rate_kafka = requests.get(
-                "http://" + prometheus_address + "/api/v1/query?query=sum(rate(kafka_server_brokertopicmetrics_messagesin_total[1m])) by (topic)")
-            true_processing = requests.get(
-                "http://" + prometheus_address + "/api/v1/query?query=avg(flink_taskmanager_job_task_numRecordsOutPerSecond) by (task_name) / (avg(flink_taskmanager_job_task_busyTimeMsPerSecond) by (task_name) / 1000)")
-            lag = requests.get(
-                "http://" + prometheus_address + "/api/v1/query?query=sum(flink_taskmanager_job_task_operator_KafkaSourceReader_KafkaConsumer_records_lag_max * flink_taskmanager_job_task_operator_KafkaSourceReader_KafkaConsumer_assigned_partitions) by (task_name)")
-
-            # previous_scaling_event = requests.get(
-            #     "http://" + prometheus_address + "/api/v1/query?query=deriv(flink_jobmanager_numRegisteredTaskManagers[" + cooldown + "])")
-            # previous_scaling_event = previous_scaling_event.json()["data"]["result"][0]["value"][1]
-            # print("taskmanager deriv: " + str(previous_scaling_event))
-
-            input_rates_per_operator = extract_per_operator_metrics(input_rate_query, include_subtask=True)
-            output_rates_per_operator = extract_per_operator_metrics(output_rate_query, include_subtask=True)
-            busy_time_per_operator = extract_per_operator_metrics(busy_time_query, include_subtask=True)
-            processors_per_operator = extract_per_operator_metrics(number_of_processors_per_task)
-            operators = list(processors_per_operator.keys())
-            input_rate_kafka = extract_per_operator_metrics(input_rate_kafka)
-            lag = extract_per_operator_metrics(lag)
-            source_true_processing = extract_per_operator_metrics(true_processing)
-
-            print("Obtained metrics")
-            print(operators)
-
-            true_processing_rate = {}
-            for key in input_rates_per_operator:
-                true_processing_rate[key] = input_rates_per_operator[key] / (busy_time_per_operator[key] / 1000)
-
-            true_output_rate = {}
-            for key in output_rates_per_operator:
-                true_output_rate[key] = output_rates_per_operator[key] / (busy_time_per_operator[key] / 1000)
-
-
-            with open('./ds2_query_data/flink_rates_' + query + '.log', 'w+', newline='') as f:
-                writer = csv.writer(f)
-                header = ["# operator_id", "operator_instance_id", "total_number_of_operator_instances", "epoch_timestamp", "true_processing_rate", "true_output_rate", "observed_processing_rate", "observed_output_rate"]
-                writer.writerow(header)
-
-                timestamp = time.time_ns()
-                for key in input_rate_kafka.keys():
-                    row = [key, 0, 1, timestamp, 1, 1, 1, 1]
-                    writer.writerow(row)
-
-                for key in input_rates_per_operator:
-                    formatted_key = key.split(" ")
-                    operator, operator_id = formatted_key[0], formatted_key[1]
-                    row = [operator, operator_id, int(processors_per_operator[operator]), timestamp, true_processing_rate[key], true_output_rate[key], input_rates_per_operator[key], output_rates_per_operator[key]]
-                    writer.writerow(row)
-            print("Wrote rates file")
-
-            operator_set = set()
-            topology_order = []
-            edges = {}
-            with open('./ds2_query_data/flink_topology_' + query + '.csv', 'w+', newline='') as csvfile:
-                reader = csv.reader(csvfile, delimiter='\n')
-                for index, row in enumerate(reader):
-                    if index == 0:
-                        continue
-                    row_values = row[0].split(",")
-                    if row_values[0] not in edges:
-                        edges[row_values[0]] = [row_values[3]]
-                    else:
-                        edges[row_values[0]].append(row_values[3])
-                    # if row_values[0] not in operator_set:
-                    #     topology_order.append(row_values[0])
-                    #     operator_set.add(row_values[0])
-                    # if row_values[3] not in operator_set:
-                    #     topology_order.append(row_values[3])
-                    #     operator_set.add(row_values[3])
-            print(edges)
-
-            lag_per_topic = {}
-            source_to_topic={"Source:_BidsSource":"bids_topic", "Source:_auctionsSource":"auction_topic", "Source:_personSource":"person_topic", "Source:_BidsSource____Timestamps_Watermarks":"bids_topic"}
-            for key, value in lag.items():
-                lag_per_topic[source_to_topic[key]] = float(value) / lag_processing_time
-
-            print("source rate")
-            print(input_rate_kafka)
-            print("extra rate due to lag")
-            print(lag_per_topic)
-
-            with open("ds2_query_data/" + query + "_source_rates.csv", 'w+', newline='') as f:
-                writer = csv.writer(f)
-                header = ["# source_operator_name","output_rate_per_instance (records/s)"]
-                writer.writerow(header)
-                for key, value in input_rate_kafka.items():
-                    row = [key, input_rate_kafka[key]]
-                    writer.writerow(row)
-            print("Wrote source rate file")
-
-            # setting number of operators for kafka topic to 1 so it can be used in topology.
-            for key in input_rate_kafka.keys():
-                processors_per_operator[key] = 1
-
-            with open('./ds2_query_data/flink_topology_' + query + '2.csv', 'w+', newline='') as f:
-                writer = csv.writer(f)
-                header = ["# operator_id1","operator_name1","total_number_of_operator_instances1","operator_id2","operator_name2","total_number_of_operator_instances2"]
-                writer.writerow(header)
-                for key in edges.keys():
-
-                    for edge in edges[key]:
-                        key = key.replace(' ', '_')
-                        edge = edge.replace(' ', '_')
-                        row = [key, key, int(processors_per_operator[key]),edge, edge,int(processors_per_operator[edge])]
-                        writer.writerow(row)
-            print("Wrote topology file")
-
-            ds2_model_result = subprocess.run(["cargo", "run", "--manifest-path", "./ds2/controller/Cargo.toml", "--release", "--bin", "policy", "--", "--topo", "ds2_query_data/flink_topology_" + query + "2.csv", "--rates", "ds2_query_data/flink_rates_" + query + ".log", "--source-rates", "ds2_query_data/" + query + "_source_rates.csv", "--system", "flink"], capture_output=True)
-            output_text = ds2_model_result.stdout.decode("utf-8").replace("\n", "")
-            output_text_values = output_text.split(",")
-            suggested_parallelism = {}
-
-            print(ds2_model_result)
-
-            filtered = []
-            for val in output_text_values:
-                if "topic" not in val:
-                    val = val.replace(" NodeIndex(0)\"", "")
-                    val = val.replace(" NodeIndex(4)\"", "")
-                    filtered.append(val)
-
-            time.sleep(cool_down)
+            job_id, path_to_savepoint = self.take_savepoint()
+            self.collect_and_write_data_to_file()
+            new_parallelism = self.call_ds2()
+            self.stop(job_id)
+            self.restart(path_to_savepoint, new_parallelism)
+            time.sleep(20)
 
 s = scheduler()
-# savepoint_path = s.take_savepoint()
-# s.restart(savepoint_path)
-s.collect_and_write_data_to_file()
+s.run()
